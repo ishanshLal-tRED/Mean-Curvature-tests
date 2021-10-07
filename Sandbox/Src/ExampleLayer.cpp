320c42516c014ad9a3213256d079cfb20cbc3e65
t-RED <lalishansh@gmail.com>
2021-10-07 22:36:22 +0530

    some clean-up and making threads safe

--- Sandbox/Src/ExampleLayer.cpp
+++ Sandbox/Src/ExampleLayer.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <mutex>
 #include <atomic>
+#include <condition_variable>
 using namespace GLCore;
 using namespace GLCore::Utils;
 
@@ -14,8 +15,7 @@ const char *ViewProjectionIdentifierInShader = "u_ViewProjectionMat4";
 const char *ModelMatrixIdentifierInShader = "u_ModelMat4";
 // Static data end
 
-bool MeanCurvatureCalculate (const std::vector<std::pair<glm::vec3, glm::vec3>> &posn_and_normals, const std::vector<GLuint> &indices, std::vector<glm::vec3> &color);
-bool MeanCurvature2Calculate (const std::vector<std::pair<glm::vec3, glm::vec3>> &posn_and_normals, const std::vector<GLuint> &indices, std::vector<glm::vec3> &color);
+bool MeanCurvatureCalculate (const char* debug_filename, const std::vector<std::pair<glm::vec3, glm::vec3>> &posn_and_normals, const std::vector<GLuint> &indices, std::vector<glm::vec3> &color, const bool displayOutput = true);
 
 bool ExampleLayer::LoadModel (std::string filePath){
 	std::vector<std::pair<glm::vec3, glm::vec3>> meshVertices;
@@ -94,11 +94,10 @@ void ExampleLayer::OnAttach()
 
 	if (m_LoadedMeshPath.empty ()) {
 		std::string filepath = "./assets/torus.obj";
-		m_LoadedMeshPath = filepath;
+		std::string temp = filepath;
 		if (!LoadModel (std::move (filepath))) {
 			LOG_ERROR ("Cannot Load Mesh");
-			m_LoadedMeshPath = "";
-		}
+		} else m_LoadedMeshPath = std::move(temp);
 	}
 	m_Camera.ReCalculateTransform ();
 	m_Camera.ReCalculateProjection (1);
@@ -155,25 +154,23 @@ void ExampleLayer::OnImGuiRender()
 			ImGui::Text ("Note:\n For some reason parsed models 1st mesh is the only one being loaded, this is 1st for me so probably a tiny mistake on my side.\nI'll Address the issue on a later date. :)");
 			ImGui::EndTabItem ();
 
-			if (ImGui::Button ("Calculate mean curvature"))
-				if (MeanCurvatureCalculate (m_StaticMeshData, m_MeshIndicesData, m_MeshColorData) && m_MeshCVB) {
+			if (ImGui::Button ("Calculate mean curvature")) {
+				uint32_t i = m_LoadedMeshPath.size ();
+				while (i > 0 && m_LoadedMeshPath[i-1] == '/' && m_LoadedMeshPath[i-1] == '\\')
+					i--;
+				if (MeanCurvatureCalculate (&m_LoadedMeshPath[i], m_StaticMeshData, m_MeshIndicesData, m_MeshColorData) && m_MeshCVB) {
 					glBindBuffer (GL_ARRAY_BUFFER, m_MeshCVB);
 					glBufferSubData (GL_ARRAY_BUFFER, 0, m_MeshColorData.size ()*sizeof (glm::vec3), m_MeshColorData.data ());
 				}
-			ImGui::SameLine ();
-			if (ImGui::Button ("Calculate mean curvature 2"))
-				if (MeanCurvature2Calculate (m_StaticMeshData, m_MeshIndicesData, m_MeshColorData) && m_MeshCVB) {
-					glBindBuffer (GL_ARRAY_BUFFER, m_MeshCVB);
-					glBufferSubData (GL_ARRAY_BUFFER, 0, m_MeshColorData.size ()*sizeof (glm::vec3), m_MeshColorData.data ());
-				}
+			}
 			if (ImGui::Button ("Load Another Model")) {
 				std::string filePath = GLCore::Utils::FileDialogs::OpenFile ("Model\0*.obj\0");
 				if (!filePath.empty ()) {
-					m_LoadedMeshPath = filePath;
-					if (!LoadModel (std::move (filePath))) {
+					std::string temp = filePath; // copy
+					if (!LoadModel (std::move (filePath)))
 						LOG_ERROR ("Cannot Load Mesh");
-						m_LoadedMeshPath = "";
-					}
+					else
+						m_LoadedMeshPath = std::move(temp);
 				}
 			}
 		}
@@ -268,15 +265,18 @@ glm::vec2 ExampleLayer::Camera::PitchYawFromDirn (glm::vec3 dirn)
 	return dirn;
 }
 
-
-bool MeanCurvatureCalculate (const std::vector<std::pair<glm::vec3, glm::vec3>> &posn_and_normals, const std::vector<GLuint>&indices, std::vector<glm::vec3> &curvature_diffuse_color)
+bool MeanCurvatureCalculate (const char* debug_filename, const std::vector<std::pair<glm::vec3, glm::vec3>> &posn_and_normals, const std::vector<GLuint> &indices, std::vector<glm::vec3> &curvature_diffuse_color, const bool trackOutput)
 {
-	float max_curvature = -std::numeric_limits<float>::max();
-	float min_curvature =  std::numeric_limits<float>::max();
-	std::vector<float> curvatures;
-	curvatures.reserve (posn_and_normals.size ());
-	const char *fn = "curvatureResults.txt";
+	float max_curvature = -std::numeric_limits<float>::max ();
+	float min_curvature =  std::numeric_limits<float>::max ();
+
+	std::vector<glm::vec3> array_K_Xi;
+	array_K_Xi.reserve (posn_and_normals.size ());
+	std::vector<float> array_K_h; // mean curvature value
+	array_K_h.reserve (posn_and_normals.size ());
+
 #if MODE_DEBUG
+	const char *fn = debug_filename != nullptr ? debug_filename : "curvatureResults.txt";
 	std::ofstream ofs (fn);
 	if (ofs.bad ()) {
 		std::cout << "File creation error - " << fn << '\n';
@@ -284,193 +284,32 @@ bool MeanCurvatureCalculate (const std::vector<std::pair<glm::vec3, glm::vec3>> 
 	}
 	std::mutex mutex_ofs;
 #endif
-	std::mutex mutex_curvature_push,mutex_cout;
-	std::atomic<size_t> vertices_processed = 0;
-	
 
-	auto mean_curvature_func = [&](uint32_t start, uint32_t stride) {
+	std::mutex mutex_curvature_push, mutex_cout, sync_lock;
+	std::condition_variable barrier;
+
+	std::atomic<size_t> vertices_processed = 0;
+	if (trackOutput)
+		std::cout << "index | A_mixed    |    curvature Kh    |    K(Xi)\n";
+	auto mean_curvature_func = [&](const uint32_t start, const uint32_t stride) {
 
 	#if MODE_DEBUG
 		std::ostringstream out_stream;
 		out_stream << std::fixed << std::setprecision (8);
 	#endif
-	
+
 		for (size_t curr_indice = start; curr_indice < posn_and_normals.size (); curr_indice += stride) // repeat for every vertex
 		{
-			mutex_cout.lock ();
-			std::cout << "\r  vertices_processed: " << vertices_processed << " out_of: " << posn_and_normals.size ();
-			mutex_cout.unlock ();
+			if (!trackOutput && curr_indice % (stride+1) == 0) {
+				mutex_cout.lock ();
+				std::cout << "\r  vertices_processed: " << vertices_processed << " out_of: " << posn_and_normals.size ();
+				mutex_cout.unlock ();
+			}
 			vertices_processed++;
 
-			float curvatureAroundVertex = 0.0f;
-			{
-				std::vector<size_t> grps; // or triangular primitives or face
-
-				std::vector<uint32_t> indices_of_neighbouring_vertives; // bucket of neighboring vertices
-
-				for (size_t i = 0; i < indices.size (); i++) { // Scan for associated primitives
-					if (indices[i] == curr_indice) {
-						uint32_t i_mod_3 = (i % 3),
-							grp = i - i_mod_3;
-						grps.push_back (grp); // note: we are assuming model is made up of triangles, so grps of 3
-						indices_of_neighbouring_vertives.push_back (indices[grp + ((i_mod_3 + 1)%3)]); // for anticlockwise iteration
-						indices_of_neighbouring_vertives.push_back (indices[grp + ((i_mod_3 + 2)%3)]); // for anticlockwise iteration
-					}
-				}
-				if (indices_of_neighbouring_vertives.empty ()) continue;
-				std::vector<uint32_t> ring{ indices_of_neighbouring_vertives[0], indices_of_neighbouring_vertives[1] };
-				bool keep_adding = true;
-				while (keep_adding && !indices_of_neighbouring_vertives.empty ()) {
-					keep_adding = false;
-
-					// forward chain extension
-					// 0-----0 - - 0 <-to_find_this
-					//       ^-is_common_vertex
-					for (auto itr = indices_of_neighbouring_vertives.begin (); itr != indices_of_neighbouring_vertives.end (); itr += 2) { // every odd position
-						if (ring.back () == *itr) { // found shared vertex
-							ring.push_back (*(itr+1)); // extend ring
-							indices_of_neighbouring_vertives.erase (itr+1);
-							indices_of_neighbouring_vertives.erase (itr);
-							keep_adding = true;
-							break;
-						}
-					}
-					// backward chain extension
-					// to_find_this->0 - - 0-----0
-					// is_common_vertex ---^
-					for (size_t i = 1; i < indices_of_neighbouring_vertives.size (); i += 2) { // every odd position
-						if (ring.front () == indices_of_neighbouring_vertives[i]) { // found shared vertex
-							ring.insert (ring.begin (), indices_of_neighbouring_vertives[i-1]); // extend ring
-							auto itr = indices_of_neighbouring_vertives.begin () + (i-1);
-							indices_of_neighbouring_vertives.erase (itr+1);
-							indices_of_neighbouring_vertives.erase (itr);
-							keep_adding = true;
-							break;
-						}
-					}
-				}
-
-				auto curvature_along_edge = [&](uint32_t curr_vertex_index, uint32_t other_vertex_index) -> float {
-					glm::vec3 nrml1 = posn_and_normals[curr_vertex_index].second;
-					glm::vec3 nrml2 = posn_and_normals[other_vertex_index].second;
-					glm::vec3 coord1 = posn_and_normals[curr_vertex_index].first;
-					glm::vec3 coord2 = posn_and_normals[other_vertex_index].first;
-					float _dot = glm::dot (nrml2 - nrml1, coord2 - coord1);
-			#if MODE_DEBUG
-					out_stream << _dot << ' ' << nrml2 << ' ' << nrml1 << ' ' << coord2 << ' ' << coord1 << '\n';
-			#endif
-					return _dot/glm::length2 (coord2 - coord1);
-				};
-			#if MODE_DEBUG
-				out_stream << "dot(norml_diff,coord_diff), norml2(vec3), norml1(vec3), coord2(vec3), coord1(vec3):\n";
-			#endif
-				std::vector<float> ring_curvatures;
-				ring_curvatures.reserve (ring.size ());
-				for (auto &other : ring) {
-					ring_curvatures.push_back (curvature_along_edge (curr_indice, other));
-				}
-			#if MODE_DEBUG
-				out_stream << "Ring, ring-curve, coord: \n";
-				for (uint32_t i = 0; i < ring.size (); i++) {
-					glm::vec3 vertex = posn_and_normals[ring[i]].first;
-					out_stream << ring[i] << ' ' << ring_curvatures[i] << '{' << vertex[0] << ", " << vertex[1] << ", " << vertex[2] << '}' << '\n';
-				}
-				//out_stream << '\n';
-			#endif
-				auto angle_between_edges = [&](uint32_t curr_vertex_index, uint32_t other1_vertex_index, uint32_t other2_vertex_index) -> float {
-					glm::vec3 edge1 = glm::normalize (posn_and_normals[other1_vertex_index].first - posn_and_normals[curr_vertex_index].first);
-					glm::vec3 edge2 = glm::normalize (posn_and_normals[other2_vertex_index].first - posn_and_normals[curr_vertex_index].first);
-					float e1e2sin_theta = glm::length (glm::cross (edge1, edge2));
-					return asinf (MIN (1.0, e1e2sin_theta));
-				};
-				float angle_accumulator = 0.0f;
-				float curvature = 0.0f;
-				for (uint32_t i = 0, __ring_size = ring.size () - 1; i < __ring_size; i++) { // now this looks ugly
-					float angle = angle_between_edges (curr_indice, ring[i], ring[i+1]);
-					angle_accumulator += angle;
-					curvature += (angle * (ring_curvatures[i] + ring_curvatures[i+1]));
-				}
-				curvatureAroundVertex = curvature/(2.0*angle_accumulator);
-			
-			#if MODE_DEBUG
-				out_stream << "total angle: " << angle_accumulator << '\n' << " idx: " << curr_indice << " curvature: " << curvatureAroundVertex << '\n';
-				out_stream << "currvertex: " << posn_and_normals[curr_indice].first << '\n' << '\n';
-			#endif
-			}
-			mutex_curvature_push.lock ();
-			curvatures.push_back (curvatureAroundVertex);
-			max_curvature = MAX (curvatureAroundVertex, max_curvature);
-			min_curvature = MIN (curvatureAroundVertex, min_curvature);
-			mutex_curvature_push.unlock ();
-		}
-		float min_max_curvature_diff_2 = (max_curvature - min_curvature)*0.5;
-	#if MODE_DEBUG
-		mutex_ofs.lock ();
-		std::cout << '\n';
-		ofs << out_stream.str();
-		mutex_ofs.unlock ();
-	#endif
-		for (size_t i = start; i < curvatures.size (); i += stride) {
-			float curvature = curvatures[i];
-			curvature -= min_curvature;
-			curvature /= min_max_curvature_diff_2;
-
-			if (curvature > 1.0) {
-				curvature -= 1.0;
-				curvature_diffuse_color[i] = glm::vec3 (1.0 - curvature, curvature, 0);
-			} else {
-				curvature_diffuse_color[i] = glm::vec3 (0, curvature, 1.0 - curvature);
-			}
-		}
-	};
-	std::vector<std::thread> threads;
-	threads.reserve (std::thread::hardware_concurrency () - 1);
-	for (uint32_t i = 1; i < std::thread::hardware_concurrency (); i++)
-		threads.emplace_back(std::move(std::thread (mean_curvature_func, i, std::thread::hardware_concurrency ())));
-	mean_curvature_func (0, std::thread::hardware_concurrency ());
-	for (auto &ref : threads)
-		ref.join ();
-#if MODE_DEBUG
-	ofs.close ();
-#endif
-	return true;
-}
-bool MeanCurvature2Calculate (const std::vector<std::pair<glm::vec3, glm::vec3>> &posn_and_normals, const std::vector<GLuint> &indices, std::vector<glm::vec3> &curvature_diffuse_color)
-{
-	float max_curvature = -std::numeric_limits<float>::max ();
-	float min_curvature = std::numeric_limits<float>::max ();
-	std::vector<glm::vec3> array_K_Xi;
-	array_K_Xi.reserve (posn_and_normals.size ());
-	std::vector<float> array_K_h; // mean curvature value
-	array_K_h.reserve (posn_and_normals.size ());
-
-#if MODE_DEBUG
-	const char *fn = "curvatureResults.txt";
-	std::ofstream ofs (fn);
-	if (ofs.bad ()) {
-		std::cout << "File creation error - " << fn << '\n';
-		__debugbreak ();
-	}
-		std::mutex mutex_ofs;
-#endif
-
-	std::mutex mutex_curvature_push, mutex_cout;
-	std::atomic<size_t> vertices_processed = 0;
-
-	std::cout << "index | A_mixed    |    curvature Kh    |    K(Xi)\n";
-	auto mean_curvature_func = [&](uint32_t start, uint32_t stride) {
-
-	#if MODE_DEBUG
-		std::ostringstream out_stream;
-		out_stream << std::fixed << std::setprecision (8);
-	#endif
-
-		for (size_t curr_indice = start; curr_indice < posn_and_normals.size (); curr_indice += stride) // repeat for every vertex
-		{
-			//mutex_cout.lock ();
-			//std::cout << "\r  vertices_processed: " << vertices_processed << " out_of: " << posn_and_normals.size ();
-			//vertices_processed++;
-			//mutex_cout.unlock ();
+		#if MODE_DEBUG
+			out_stream << "\nIDX: " << curr_indice << '\n';
+		#endif
 
 			glm::vec3 K_Xi = glm::vec3 (0);
 			{
@@ -524,6 +363,13 @@ bool MeanCurvature2Calculate (const std::vector<std::pair<glm::vec3, glm::vec3>>
 				}
 
 
+			#if MODE_DEBUG
+				out_stream << "RING: ";
+				for (auto val : ring)
+					out_stream << std::setw (5) << val << ' ';
+				out_stream << '\n';
+			#endif
+
 				auto angle_between_edges = [&](uint32_t curr_vertex_index, uint32_t other1_vertex_index, uint32_t other2_vertex_index) -> float {
 					glm::vec3 edge1 = glm::normalize (posn_and_normals[other1_vertex_index].first - posn_and_normals[curr_vertex_index].first);
 					glm::vec3 edge2 = glm::normalize (posn_and_normals[other2_vertex_index].first - posn_and_normals[curr_vertex_index].first);
@@ -536,13 +382,14 @@ bool MeanCurvature2Calculate (const std::vector<std::pair<glm::vec3, glm::vec3>>
 				//     /  \
 				//   Q/____\R
 				float A_mixed = 0;
-				glm::vec3 sigma_mean_curvature_normal_operator = glm::vec3(0);
+				glm::vec3 sigma_mean_curvature_normal_operator = glm::vec3 (0);
 				for (uint32_t i = 1; i < ring.size (); i++) {
 					float Q = angle_between_edges (ring[i-1], ring[i], curr_indice)
 						, R = angle_between_edges (ring[i], curr_indice, ring[i-1])
 						, X = angle_between_edges (curr_indice, ring[i], ring[i-1]);
 					float pXR2, pXQ2, cot_Q, cot_R;
-					glm::vec3 XR_diff, XQ_diff; {
+					glm::vec3 XR_diff, XQ_diff;
+					{
 						XR_diff = posn_and_normals[curr_indice].first - posn_and_normals[ring[i-1]].first;
 						XQ_diff = posn_and_normals[curr_indice].first - posn_and_normals[ring[i]].first;
 						cot_Q = cotf (Q), cot_R = cotf (R);
@@ -564,11 +411,16 @@ bool MeanCurvature2Calculate (const std::vector<std::pair<glm::vec3, glm::vec3>>
 						= cot_Q*XR_diff +  cot_R*XQ_diff;
 					sigma_mean_curvature_normal_operator += mean_curvature_normal_operator_for_curr_tri;
 				}
-				K_Xi = (sigma_mean_curvature_normal_operator)*float(1.0/(2.0*A_mixed));
+				K_Xi = (sigma_mean_curvature_normal_operator)*float (1.0/(2.0*A_mixed));
 
-				mutex_cout.lock ();
-				std::cout << std::setw (4) << curr_indice << ' ' << A_mixed << ' ' << glm::length (K_Xi)*0.5  << ' '<< K_Xi << '\n';
-				mutex_cout.unlock ();
+				if (trackOutput) {
+					mutex_cout.lock ();
+					std::cout << std::setw (4) << curr_indice << ' ' << A_mixed << ' ';
+					mutex_cout.unlock ();
+				}
+			#if MODE_DEBUG
+				out_stream << "A_mixed: " << A_mixed;
+			#endif
 			}
 			mutex_curvature_push.lock ();
 			array_K_Xi.push_back (K_Xi);
@@ -576,15 +428,35 @@ bool MeanCurvature2Calculate (const std::vector<std::pair<glm::vec3, glm::vec3>>
 			array_K_h.push_back (K_h);
 			max_curvature = MAX (K_h, max_curvature);
 			min_curvature = MIN (K_h, min_curvature);
+			if (trackOutput) {
+				//mutex_cout.lock ();
+				std::cout << K_h << ' ' << K_Xi << '\n';
+				//mutex_cout.unlock ();
+			}
 			mutex_curvature_push.unlock ();
 
 		#if MODE_DEBUG
-			out_stream << " idx: " << curr_indice << " mean_curvature_val: " << K_h << " Mean Curvature Norml: " << K_Xi << '\n';
-			out_stream << "currvertex: " << posn_and_normals[curr_indice].first << " normal: " << posn_and_normals[curr_indice].second << '\n' << '\n';
+			out_stream << " mean_curvature: " << K_h << " K(Xi) " << K_Xi << "\ncurrvertex: " << posn_and_normals[curr_indice].first << " normal: " << posn_and_normals[curr_indice].second << '\n';
 		#endif
 		}
+
+
+		// Sync threads
+		__debugbreak ();
+		std::unique_lock ul(sync_lock);
+		if (vertices_processed == posn_and_normals.size ())
+			barrier.notify_all ();
+		else barrier.wait (ul);
+
 		float min_max_curvature_diff = (max_curvature - min_curvature);
+
 	#if MODE_DEBUG
+		if (start == 0) { // only 1st one is allowed to write
+			ofs << "mean_curvatures{max: " << max_curvature << ", min: " << min_curvature << "}\n\n";
+			std::cout << "\nresulting mean_curvatures{max: " << max_curvature << ", min: " << min_curvature << "}\n\n";
+		} else barrier.wait (ul);
+		barrier.notify_all ();
+
 		mutex_ofs.lock ();
 		ofs << out_stream.str ();
 		mutex_ofs.unlock ();
